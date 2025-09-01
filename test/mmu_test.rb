ef70e6d7ded258fae4e794755f349c2c4bc9ac96
Nick Vladimiroff <nickvladimiroff@gmail.com>
2025-09-01 08:48:01 -0400

    MMU: more tests.

---
+++ test/mmu_test.rb
@@ -0,0 +1,16 @@
+require_relative './test_helper'
+
+class MMUTest < Minitest::Test
+
+  def test_shadow_wram
+    run_program do
+      ld bc, 0xC000
+      ld a, 0xFF
+      ld [bc], a
+    end
+
+    assert_equal(0xFF, @gb.mmu[0xC000])
+    assert_equal(0xFF, @gb.mmu[0xE000])
+  end
+
+end
