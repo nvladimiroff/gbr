5a889228b525e7d2c4e188c6f359dc5d31623fc6
Nick Vladimiroff <nickvladimiroff@gmail.com>
2025-09-07 12:02:43 -0400

    PPU: the other easy test.

--- test/ppu_test.rb
+++ test/ppu_test.rb
@@ -16,8 +16,26 @@ class PPUTest < Minitest::Test
       halt # wait for vblank
     end
 
-    assert_equal(0xFF, @gb.mmu[0x803F])
     assert(@gb.ppu.pixels.all? do |p| p == 0x005121FF end)
   end
+
+
+  def test_render_white_screen
+    run_program do
+      ld hl, 0x8000
+      ld a, 0x00
+      ld b, 0x40
+
+      nop
+      ldi [hl], a
+      dec b
+      jr nz, 0xFB
+
+      halt # wait for vblank
+    end
+
+    assert(@gb.ppu.pixels.all? do |p| p == 0x578200FF end)
+  end
+
 
 end
