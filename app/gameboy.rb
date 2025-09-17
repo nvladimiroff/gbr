8cacd1b5faccbc557bf0fe55f3b9c6369ab2f38f
Nick Vladimiroff <nickvladimiroff@gmail.com>
2025-09-16 20:13:57 -0400

    Debugger: remove the old console debug code.

--- app/gameboy.rb
+++ app/gameboy.rb
@@ -30,31 +30,6 @@ class Gameboy
   end
 
 
-  def dump_state(memory_range = 0x100..0x14F)
-    Console.new.display_until_quit do |out|
-      out.section("REGISTERS") do
-        out.row(*%w(PC SP A B C D E F H L))
-        out.row(*[@cpu.pc, @cpu.sp, @cpu.a, @cpu.b, @cpu.c, @cpu.d, @cpu.e, @cpu.f, @cpu.h, @cpu.l].map(&:to_hex))
-      end
-
-      out.section("INSTRUCTIONS") do
-        out.line("Last instruction executed: #{CPU::Opcodes::MAPPING[@cpu.op]}")
-        out.line("Next instruction: #{CPU::Opcodes::MAPPING[@mmu[@cpu.pc+1]]}")
-      end
-
-      out.section("INTERRUPTS") do
-        out.line("Pending interrupts: #{@interrupts[0xFF0F].to_hex}")
-      end
-
-      out.section("MEMORY") do
-        memory_range.each_slice(16) do |group|
-          out.row(*group.map { |addr| @mmu[addr].to_hex })
-        end
-      end
-    end
-  end
-
-
   private
 
     def print_debug_info
