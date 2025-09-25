d29d96276879930b3cb406ed5fbf8eb6b661bf24
Nick Vladimiroff <nickvladimiroff@gmail.com>
2025-09-24 23:12:17 -0400

    MMU: use if/else because it's much faster.

--- app/mmu.rb
+++ app/mmu.rb
@@ -16,96 +16,93 @@ class MMU
 
 
   def [](addr)
-    case addr
-    when 0x0..0x7FFF
+    if addr >= 0x0 && addr <= 0x7FFF
       @cartridge[addr]
-    when 0x8000..0x9FFF
+    elsif addr >= 0x8000 && addr <= 0x9FFF
       # VRAM
       @ppu[addr]
-    when 0xA000..0xBFFF
+    elsif addr >= 0xA000 && addr <= 0xBFFF
       @cartridge[addr]
-    when 0xC000..0xDFFF
+    elsif addr >= 0xC000 && addr <= 0xDFFF
       @wram[addr - 0xC000]
-    when 0xE000..0xFDFF
+    elsif addr >= 0xE000 && addr <= 0xFDFF
       # WRAM (Shadow)
       @wram[addr - 0xE000]
-    when 0xFE00..0xFE9F
+    elsif addr >= 0xFE00 && addr <= 0xFE9F
       # Sprites
       @ppu[addr]
-    when 0xFEA0..0xFEFF
+    elsif addr >= 0xFEA0 && addr <= 0xFEFF
       # Unusable
       0xFF
-    when 0xFF00
+    elsif addr == 0xFF00
       @joypad.read
-    when 0xFF01..0xFF02
+    elsif addr >= 0xFF01 && addr <= 0xFF02
       # TODO: Serial communication
       0xFF
-    when 0xFF04..0xFF07
+    elsif addr >= 0xFF04 && addr <= 0xFF07
       @timers[addr]
-    when 0xFF0F
+    elsif addr == 0xFF0F
       @cpu.if
-    when 0xFF10..0xFF3F
+    elsif addr >= 0xFF10 && addr <= 0xFF3F
       # TODO: Sound
       0xFF
-    when 0xFF40..0xFF4B
+    elsif addr >= 0xFF40 && addr <= 0xFF4B
       @ppu[addr]
-    when 0xFF00..0xFF7F
+    elsif addr >= 0xFF00 && addr <= 0xFF7F
       # TODO: Other IO I haven't implemented yet.
       #$logger.warn('Unimplemented memory read', addr: addr.to_hex)
       0xFF
-    when 0xFF80..0xFFFE
+    elsif addr >= 0xFF80 && addr <= 0xFFFE
       @zram[addr - 0xFF80]
-    when 0xFFFF
+    elsif addr == 0xFFFF
       @cpu.ie
     end
   end
 
 
   def []=(addr, value)
-    case addr
-    when 0x0..0x7FFF
+    if addr >= 0x0 && addr <= 0x7FFF
       @cartridge[addr] = value
-    when 0x8000..0x9FFF
+    elsif addr >= 0x8000 && addr <= 0x9FFF
+      # VRAM
       @ppu[addr] = value
-    when 0xA000..0xBFFF
+    elsif addr >= 0xA000 && addr <= 0xBFFF
       @cartridge[addr] = value
-    when 0xC000..0xDFFF
+    elsif addr >= 0xC000 && addr <= 0xDFFF
       @wram[addr - 0xC000] = value
-    when 0xE000..0xFDFF
+    elsif addr >= 0xE000 && addr <= 0xFDFF
       # WRAM (Shadow)
       @wram[addr - 0xE000] = value
-    when 0xFE00..0xFE9F
+    elsif addr >= 0xFE00 && addr <= 0xFE9F
       # Sprites
       @ppu[addr] = value
-    when 0xFEA0..0xFEFF
+    elsif addr >= 0xFEA0 && addr <= 0xFEFF
       # Unusable
-    when 0xFF00
+    elsif addr == 0xFF00
       @joypad.write(value)
-    when 0xFF01
+    elsif addr >= 0xFF01 && addr <= 0xFF02
       STDOUT.write(value.chr)
-    when 0xFF02
-      # TODO: Serial communication
-    when 0xFF04..0xFF07
+    elsif addr >= 0xFF04 && addr <= 0xFF07
       @timers[addr] = value
-    when 0xFF0F
+    elsif addr == 0xFF0F
       @cpu.if = value
-    when 0xFF10..0xFF3F
+    elsif addr >= 0xFF10 && addr <= 0xFF3F
       # TODO: Sound
-    when 0xFF46
+    elsif addr == 0xFF46
       # DMA
       # TODO (low): this actually progresses at one byte per 4 cycles.
       location = value << 8
       0xA0.times do |i|
         @ppu[0xFE00 + i] = self[location + i]
       end
-    when 0xFF40..0xFF4B
+    elsif addr >= 0xFF40 && addr <= 0xFF4B
       @ppu[addr] = value
-    when 0xFF00..0xFF7F
+    elsif addr >= 0xFF00 && addr <= 0xFF7F
       # TODO: Other IO I haven't implemented yet.
-      #$logger.warn('Unimplemented memory write', addr: addr.to_hex, value: value.to_hex)
-    when 0xFF80..0xFFFE
+      #$logger.warn('Unimplemented memory write', addr: addr.to_hex)
+    elsif addr >= 0xFF80 && addr <= 0xFFFE
       @zram[addr - 0xFF80] = value
-    when 0xFFFF
+    elsif addr == 0xFFFF
       @cpu.ie = value
     end
   end
